9fd131871e2ea436d61b953a1f389f96bb3e4ecc
Maxime Gariepy <maxime.gariepy.ahuntsic@gmail.com>
2024-06-26 06:23:21 -0400

    photo medaillon aux pages fonctionnelles

--- Serveur/BD/obj/Debug/net8.0/BD.AssemblyInfo.cs
+++ Serveur/BD/obj/Debug/net8.0/BD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3b046f11da4614f57943754ec220a93951252c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
